5d7901b84986e8c87ecf15b2a7383a46e33c1746
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-07 11:22:02 +0900

    Providing with some sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create!('name':'task 1')
+Task.create!('name':'task 2')
+Task.create!('name':'task 3')
+Task.create!('name':'task 4')
