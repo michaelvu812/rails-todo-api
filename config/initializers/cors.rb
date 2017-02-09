fdf5d8e8a8270dad76e1ca67e839dcce47b7b73b
Askar Karasaev <askar.karasaev@gmail.com>
2017-02-09 13:25:06 +0900

    cors updated with vagrant 10.20.30.100:4200

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:4200'
+    origins '10.20.30.101:4200'
 
     resource '*',
       headers: :any,
